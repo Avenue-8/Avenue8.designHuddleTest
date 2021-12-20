7cc3d87de15678059902d1e6f147a280d4110ab4
mourodrigo <rodrigobt20@gmail.com>
2021-12-20 17:49:02 -0300

    Initial Commit

---
+++ designHuddleTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  designHuddleTest
+//
+//  Created by Rodrigo Bueno Tomiosso on 20/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
